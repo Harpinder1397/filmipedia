d2261497a9890a171d9546f96365a72e2d61ae2a
Harpinder Bhullar <harpinder0@Harpinders-MacBook-Air.local>
2023-01-04 01:58:05 +0530

    done

--- src/main.jsx
+++ src/main.jsx
@@ -8,19 +8,15 @@ import {
   QueryClientProvider,
 } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
-import { Provider } from 'react-redux';
-import store from "./store/store"
 
 const queryClient = new QueryClient()
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
         <QueryClientProvider client={queryClient}>
             <ReactQueryDevtools initialIsOpen={false} />
             <App />
         </QueryClientProvider>
-      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
